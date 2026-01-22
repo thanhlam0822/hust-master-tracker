0bc5c0eb04fb41737c3de4f5570e4b5fa12a6727
LamCT <thanhlam_chu@vnpt.vn>
2026-01-22 10:16:58 +0700

    draft code

--- src/layout/TrackerDashboard.vue
+++ src/layout/TrackerDashboard.vue
@@ -44,10 +44,10 @@
       </div>
       <div>
         <card>
-          <p class="common-title">Khối kiến thức ngành rộng (12 TC)</p>
+          <p class="common-title">Khối kiến thức chuyên ngành nâng cao (18TC)</p>
           <div class="course-list">
             <course-item
-                v-for="course in broadDisciplinaryKnowledgeBlock"
+                v-for="course in advancedSpecializedKnowledgeBlock"
                 :key="course.id"
                 :code="course.code"
                 :name="course.name"
@@ -60,21 +60,46 @@
       </div>
       <div>
         <card>
-          <p class="common-title">Khối kiến thức ngành rộng (12 TC)</p>
-          <div class="course-list grid-cols-3">
-            <course-item
-                v-for="course in broadDisciplinaryKnowledgeBlock"
-                :key="course.id"
-                :code="course.code"
-                :name="course.name"
-                :credits="course.credit"
-                :icon="course.icon"
-                v-model="course.done"
-            />
+          <p class="common-title">Khối mô-đun định hướng ứng dụng (12TC) - Chọn 1</p>
+          <div class="custom-course-list-container no-bottom-border">
+            <div class="custom-title">
+              LỰA CHỌN A: Công nghệ phần mềm
+            </div>
+            <div class="course-list no-margin-top grid-cols-3">
+              <course-item
+                  v-for="course in broadDisciplinaryKnowledgeBlock"
+                  :key="course.id"
+                  :code="course.code"
+                  :name="course.name"
+                  :credits="course.credit"
+                  :icon="course.icon"
+                  v-model="course.done"
+              />
+            </div>
+          </div>
+
+          <div class="custom-course-list-container no-margin-top">
+            <div class="custom-title">
+              LỰA CHỌN B: Hệ thống thông tin
+            </div>
+            <div class="course-list no-margin-top grid-cols-3">
+              <course-item
+                  v-for="course in broadDisciplinaryKnowledgeBlock"
+                  :key="course.id"
+                  :code="course.code"
+                  :name="course.name"
+                  :credits="course.credit"
+                  :icon="course.icon"
+                  v-model="course.done"
+              />
+            </div>
+            <div class="note">
+              Lưu ý: Chọn 1 trong 2 mô đun và hoàn thành toàn bộ các môn trong mô đun đã chọn
+            </div>
           </div>
         </card>
         <card>
-          <p class="common-title">Khối kiến thức ngành rộng (12 TC)</p>
+          <p class="common-title">Thực tập và luận văn tốt nghiệp (15 TC)</p>
           <div class="course-list grid-cols-3">
             <course-item
                 v-for="course in broadDisciplinaryKnowledgeBlock"
@@ -108,6 +133,16 @@ const broadDisciplinaryKnowledgeBlock = ref([
   {id: 5, code: 'IT6819', name: 'Kiến trúc phần mềm', credit: 2, icon: 'codesandbox'},
   {id: 6, code: 'IT6340', name: 'Các hệ thống thông minh', credit: 2, icon: 'brain'},
   {id: 7, code: 'IT5440', name: 'Nguyên lý và kỹ thuật phân tích chương trình', credit: 2, icon: 'document'}
+])
+
+const advancedSpecializedKnowledgeBlock = ref([
+  {id: 8, code: 'IT4922', name: 'Mạng thế hệ sau', credit: 2, icon: 'network'},
+  {id: 9, code: 'IT4527', name: 'Blockchain và ứng dụng', credit: 2, icon: 'coin'},
+  {id: 10, code: 'IT4772', name: 'Xử lý ngôn ngữ tự nhiên', credit: 3, icon: 'brain'},
+  {id: 11, code: 'IT5419', name: 'Tích hợp hệ thống thông tin', credit: 3, icon: 'network'},
+  {id: 12, code: 'IT6390', name: 'Web ngữ nghĩa', credit: 3, icon: 'globe'},
+  {id: 13, code: 'IT4235', name: 'Thị giác máy tính', credit: 2, icon: 'scan_eye'},
+  {id: 14, code: 'IT5550', name: 'Các giải pháp và chính sách an ninh mạng', credit: 3, icon: 'graduate'}
 ])
 </script>
 <style scoped lang="scss">
@@ -158,6 +193,11 @@ const broadDisciplinaryKnowledgeBlock = ref([
     display: grid;
     gap: 16px;
   }
+
+  .custom-title {
+    padding: 8px 0;
+    font-weight: bold;
+  }
 }
 
 :deep(.card) {
@@ -175,5 +215,25 @@ const broadDisciplinaryKnowledgeBlock = ref([
 
 .grid-cols-3 {
   grid-template-columns: repeat(3, 1fr);
+}
+
+.custom-course-list-container {
+  height: 100%;
+  width: 100%;
+  border: 2px dashed #d3d3d3;
+  padding: 0 20px 10px 20px;
+  margin-top: 10px;
+}
+
+.custom-course-list-container.no-bottom-border {
+  border-bottom: none;
+}
+
+.no-margin-top {
+  margin-top: 0 !important;
+}
+
+.note {
+  font-style: italic;
 }
 </style>
